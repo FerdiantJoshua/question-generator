91f0294feef1627aa47a50c4be9f38a1494734a1
Unknown <ferdiant.98@gmail.com>
2020-03-11 23:12:38 +0700

    Restructure project

--- dataset_parser.py
+++
@@ -1,5 +0,0 @@
-with open('Datasets/SQuAD/v2.0/dev-v2.0.json', 'r') as f_in:
-    buffer = f_in.read(1024000)
-    print(buffer)
-    with open('Datasets/SQuAD/v2.0/dev-v2.0-test.txt', 'w') as f_out:
-        f_out.write(buffer)
