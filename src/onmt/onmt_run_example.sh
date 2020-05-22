48e4210687e39384c49b1c9419e4d89e612bcb66
FerdiantJoshua <ferdiant.98@gmail.com>
2020-05-22 16:13:55 +0700

    Update onmt_run_example.sh

--- src/onmt/onmt_run_example.sh
+++ src/onmt/onmt_run_example.sh
@@ -1,23 +1,23 @@
 #================================================== PREPROCESS ==================================================
-onmt_preprocess -train_src 'data/train/squad_id_cased_source.txt' -'train_tgt data/train/squad_id_cased_target.txt' \
-    -valid_src 'data/val/squad_id_cased_source.txt' -valid_tgt 'data/val/squad_id_cased_target.txt' \
+onmt_preprocess -train_src 'data/onmt/train/squad_id_cased_source.txt' -train_tgt 'data/onmt/train/squad_id_cased_target.txt' \
+    -valid_src 'data/onmt/val/squad_id_cased_source.txt' -valid_tgt 'data/onmt/val/squad_id_cased_target.txt' \
     -dynamic_dict \
     -save_data 'data/onmt/squad_id_cased' \
     -overwrite \
-    -src_vocab_size 60000 \
+    -src_vocab_size 50000 \
     -tgt_vocab_size 30000 \
     -src_seq_length 60 \
     -tgt_seq_length 20
 
 #================================================== EMBEDDING ==================================================
-./tools/embeddings_to_torch.py -emb_file_both 'models/word-embedding/ft_to_gl_300_id.vec' \
-    -dict_file '../data/onmt/squad_id_cased.vocab.pt' \
-    -output_file '../data/onmt/embeddings_cased'
+python src/onmt/embeddings_to_torch.py -emb_file_both 'models/word-embedding/ft_to_gl_300_id.vec' \
+    -dict_file 'data/onmt/squad_id_cased.vocab.pt' \
+    -output_file 'data/onmt/embeddings_cased'
 
 #================================================== TRAIN ==================================================
 #-------------------------------------------------- RNN --------------------------------------------------
-!onmt_train -data 'data/onmt/squad_id_cased' -save_model 'models/checkpoints/onmt/lstm_001' \
-    -world_size 1 -gpu_ranks 0 \
+onmt_train -data 'data/onmt/squad_id_cased' -save_model 'models/checkpoints/onmt/lstm_011' \
+    -world_size 4 -gpu_ranks 0 1 2 3 \
     -save_checkpoint_steps 7195 \
     -word_vec_size 300 \
     -pre_word_vecs_enc 'data/onmt/embeddings_cased.enc.pt' \
@@ -40,9 +40,8 @@ onmt_preprocess -train_src 'data/train/squad_id_cased_source.txt' -'train_tgt da
     -batch_size 64 \
     -dropout 0.3
 #-------------------------------------------------- TRANSFORMER --------------------------------------------------
-!onmt_train -data 'data/onmt/squad_id_cased' -save_model 'output/transformer_001' \
+onmt_train -data 'data/onmt/squad_id_cased' -save_model 'models/checkpoints/onmt/transformer_004' \
     -world_size 1 -gpu_ranks 0 \
-    -train_from 'models/checkpoints/onmt/lstm_001_step_35975.pt' \
     -pre_word_vecs_enc 'data/onmt/embeddings_cased.enc.pt' \
     -pre_word_vecs_dec 'data/onmt/embeddings_cased.dec.pt' \
     -fix_word_vecs_enc \
@@ -52,8 +51,8 @@ onmt_preprocess -train_src 'data/train/squad_id_cased_source.txt' -'train_tgt da
     -layers 2 -rnn_size 300 -word_vec_size 300 -transformer_ff 256 -heads 2 \
     -encoder_type transformer -decoder_type transformer -position_encoding \
     -train_steps 200000  -max_generator_batches 2 -dropout 0.1 \
-    -batch_size 128 -batch_type tokens -normalization tokens  -accum_count 2 \
-    -optim adam -adam_beta2 0.998 -decay_method noam -warmup_steps 4000 -learning_rate 2 \
+    -batch_size 256 -batch_type tokens -normalization tokens  -accum_count 2 \
+    -optim adam -adam_beta2 0.998 -decay_method noam -warmup_steps 8000 -learning_rate 2 \
     -max_grad_norm 0 -param_init 0  -param_init_glorot \
     -label_smoothing 0.1 -valid_steps 1200 -save_checkpoint_steps 3000 \
     -copy_attn \
