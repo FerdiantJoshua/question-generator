ef4783211fd3b296a27bc07099ff6368542caf5f
FerdiantJoshua <ferdiant.98@gmail.com>
2020-05-23 08:43:50 +0700

    Add src/pure-pytorch

---
+++ src/pure-pytorch/scripts/Copy_Server_Image_To_Local.bat
@@ -0,0 +1,2 @@
+@echo off
+scp ayu@167.205.35.252:/home/ayu/TugasAkhir_13516047_FerdiantJM/question-generator/figures.jpg reports\figures\figures.jpg
---
+++ src/pure-pytorch/scripts/Copy_Source_Code_To_Server.bat
@@ -0,0 +1,2 @@
+@echo off
+scp src\models\question_generator.py ayu@167.205.35.252:/home/ayu/TugasAkhir_13516047_FerdiantJM/question-generator/src/models/.
